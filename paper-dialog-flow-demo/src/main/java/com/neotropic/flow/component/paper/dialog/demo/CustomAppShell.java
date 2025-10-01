28beb80bd5641b6b1b9dc6d114654ea39c62a179
jcamachoe <julian.camacho@kuwaiba.org>
2025-10-01 14:47:49 -0500

    Initial commit: Snapshot of codebase from Subversion [(7629)] (https://sourceforge.net/p/kuwaiba/code/HEAD/tree/components/paper-dialog-flow/branches/version-24.6.2/)

---
+++ paper-dialog-flow-demo/src/main/java/com/neotropic/flow/component/paper/dialog/demo/CustomAppShell.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neotropic.flow.component.paper.dialog.demo;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.")
+public class CustomAppShell implements AppShellConfigurator {
+    
+}
+
